222632d5166956926e5ca58e1b0f4c3b1a72167c
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-03-11 15:47:48 +0530

    updated user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const {
-  userRegistration,
+  userRegister,
   userLogin,
 } = require("../controllers/user.controllers");
 
 const router = express.Router();
 
-router.route("/register").post(userRegistration);
+router.route("/register").post(userRegister);
 router.route("/login").post(userLogin);
 
 module.exports = router;
